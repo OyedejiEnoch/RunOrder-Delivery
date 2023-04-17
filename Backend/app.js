bcdfd5db6a4213eed2abd44eaa93ed8822b0ce9a
Oyedeji Enoch <OyedejiEnoch@gmail.com>
2023-04-17 23:27:16 +0100

    added routed

--- Backend/app.js
+++ Backend/app.js
@@ -4,12 +4,12 @@ const cookieParser = require("cookie-parser")
 const bodyParser = require("body-parser")
 const cloudinary = require("cloudinary")
 const fileupload = require("express-fileupload");
-// const cors = require('cors')
+const cors = require('cors')
 
 
 const errorMiddleWares = require("./middleWares/errors")
 
-// app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 5000 }));
